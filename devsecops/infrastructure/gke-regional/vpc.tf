328f2b008f8ad5af5a3d8537045a1a0e91456e1c
Horizon <omkar@anthaathi.org>
2022-04-29 11:53:33 +0400

    cicd: fix variable for the network

--- devsecops/infrastructure/gke-regional/vpc.tf
+++ devsecops/infrastructure/gke-regional/vpc.tf
@@ -3,12 +3,10 @@ provider "google" {
   region  = var.region
 }
 
-
-
 # Subnet
 resource "google_compute_subnetwork" "subnet" {
   name          = "${var.project_id}-subnet"
   region        = var.region
-  network       = google_compute_network.vpc.name
+  network       = var.google_compute_network_vpc_name
   ip_cidr_range = "10.10.0.0/24"
 }
