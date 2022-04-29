e0b9883784a1bc9d6aaf60e9b419efb8acb892aa
Horizon <omkar@anthaathi.org>
2022-04-29 11:57:42 +0400

    cicd: added project id to the vpc development

--- devsecops/infrastructure/vpc.tf
+++ devsecops/infrastructure/vpc.tf
@@ -2,4 +2,5 @@
 resource "google_compute_network" "vpc_development" {
   name                    = "${var.project_id}-vpc"
   auto_create_subnetworks = "false"
+  project                 = var.project_id
 }
