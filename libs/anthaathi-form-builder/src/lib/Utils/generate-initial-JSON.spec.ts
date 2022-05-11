9ab5bea6940862afad88180255e5bdc632571fa8
Horizon <omkar@anthaathi.org>
2022-05-11 14:30:38 +0400

    codecov: added code coverage

--- libs/anthaathi-form-builder/src/lib/Utils/generate-initial-JSON.spec.ts
+++ libs/anthaathi-form-builder/src/lib/Utils/generate-initial-JSON.spec.ts
@@ -4,7 +4,12 @@ describe('Generate initial JSON', () => {
   it('should render the items', () => {
     const data = generateInitialJSON({
       type: 'object',
-      properties: { kind: { type: 'string' }, abs: { type: 'object' } },
+      properties: {
+        kind: { type: 'string' },
+        abs: { type: 'object' },
+        test: { type: 'array' },
+        test2: { type: 'number', default: 0 },
+      },
     });
 
     expect(data).toMatchSnapshot();
