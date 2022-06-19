afbc480f0c9f5c8e61bfc88e4631dde71a8ec8a9
Horizon <omkar@anthaathi.org>
2022-06-19 23:28:21 +0400

    setup initial arango query builder

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,3 +1,4 @@
 object Versions {
     const val KOTLIN_VERSION = "1.6.10"
+    const val ARANGO_DB_DRIVER_VERSION = "6.18.0"
 }
---
+++ libs/anthaathi-arango-query-builder/src/main/kotlin/org/anthaathi/arango_query_builder/QueryBuilder.kt
@@ -0,0 +1,7 @@
+package org.anthaathi.arango_query_builder
+
+import com.arangodb.ArangoDatabase
+
+
+class QueryBuilder(var arango: ArangoDatabase) {
+}
