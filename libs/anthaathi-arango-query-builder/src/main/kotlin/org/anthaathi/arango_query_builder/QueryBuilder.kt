ae8b8179406a25b5434a6de4b31fa53f32c8416a
omkar <omkar@anthaathi.org>
2022-06-24 19:35:38 +0400

    feat: added plugin for graphql-engine

--- libs/anthaathi-arango-query-builder/src/main/kotlin/org/anthaathi/arango_query_builder/QueryBuilder.kt
+++
@@ -1,7 +0,0 @@
-package org.anthaathi.arango_query_builder
-
-import com.arangodb.ArangoDatabase
-
-
-class QueryBuilder(var arango: ArangoDatabase) {
-}
---
+++ libs/graphql-engine/deployment/target/generated-sources/annotations/org/anthaathi/graphql/engine/deployment/GraphqlEngineProcessor$$accessor.java
@@ -0,0 +1,7 @@
+package org.anthaathi.graphql.engine.deployment;
+public final class GraphqlEngineProcessor$$accessor {
+    private GraphqlEngineProcessor$$accessor() {}
+    public static Object construct() {
+        return new GraphqlEngineProcessor();
+    }
+}
