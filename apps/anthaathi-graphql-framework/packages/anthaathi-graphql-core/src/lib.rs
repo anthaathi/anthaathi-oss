80e5fd80f77bcdf1c72a820c89272d8abad58da0
Horizon <omkar@anthaathi.org>
2022-07-02 11:52:51 +0400

    feat: move the things

--- apps/anthaathi-graphql-framework/packages/anthaathi-graphql-core/src/lib.rs
+++ apps/anthaathi-graphql-framework/packages/anthaathi-graphql-core/src/lib.rs
@@ -1,5 +1,10 @@
 pub mod anthaathi_graphql_core {
-    use anthaathi_graphql_common::config_loader::{parse_config, Config};
+    use anthaathi_graphql_common::config_loader::parse_config;
+    use hyper::service::{make_service_fn, service_fn};
+    use hyper::{Body, Request, Response, Server};
+    use std::convert::Infallible;
+    use std::net::SocketAddr;
+    use std::ptr::null;
 
     pub trait GraphQLServer {}
 
@@ -7,10 +12,32 @@ pub mod anthaathi_graphql_core {
 
     impl GraphQLServer for GQLServer {}
 
+    async fn handle(_: Request<Body>) -> Result<Response<Body>, Infallible> {
+        Ok(Response::new("Hello, World!".into()))
+    }
+
+    async fn setup_port() -> Result<_, serde_yaml::Error> {
+        let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+        let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle)) });
+
+        let server = Server::bind(&addr).serve(make_svc);
+
+        if let Err(e) = server.await {
+            eprintln!("server error: {}", e);
+        }
+
+        return Ok(null());
+    }
+
     pub async fn init_server(config_input: &str) -> Result<impl GraphQLServer, serde_yaml::Error> {
         let config = parse_config(config_input)?;
 
         let server = GQLServer {};
+
+        for x in config.spec {
+            x.port;
+        }
 
         return Ok(server);
     }
