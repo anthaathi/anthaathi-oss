635775f0144f3daea2a957d18d36a40d809565e0
kedar09 <httpskedar@gmail.com>
2022-07-03 12:01:02 +0530

    delivert date, timeslot component, blogpost component

---
+++ apps/anthaathi-commerce-mobile-client/src/features/CMS/containers/CheckOutPage/components/TimeSlotSelection/index.spec.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import 'react-native';
+import {render} from '@testing-library/react-native';
+import {IntlProvider} from 'react-intl';
+
+import locale from '../../../../../../compiled-locales/en-US.json';
+import {ThemeProvider} from 'react-native-paper';
+import TimeSlot from './index';
+
+describe('TimeSlot', () => {
+  it('should render the item', function () {
+    const temp = render(
+      <ThemeProvider>
+        <IntlProvider locale="en-US" messages={locale}>
+          <TimeSlot
+            title="Timeslot"
+            timeSlots={[
+              {
+                key: '1',
+                name: '09am - 12pm',
+              },
+              {
+                key: '2',
+                name: '01pm - 05pm',
+              },
+              {
+                key: '3',
+                name: '05pm - 10pm',
+              },
+            ]}
+          />
+        </IntlProvider>
+      </ThemeProvider>,
+    );
+
+    expect(temp).toMatchSnapshot();
+    expect(temp.queryByTestId('timeSlot')).toBeTruthy();
+  });
+});
---
+++ apps/anthaathi-commerce-mobile-client/src/features/CMS/containers/HomePage/components/BlogPosts/index.tsx
@@ -0,0 +1,127 @@
+import {Image, Pressable, View} from 'react-native';
+import React from 'react';
+import {Divider, Text} from 'react-native-paper';
+import {useIntl} from 'react-intl';
+import {useResponsiveValue} from '../../../../utils/useResponsiveValue';
+import {useDimension} from '../../../../utils/useDimension';
+
+type BlogsProps = {
+  id: number;
+  title: string;
+  image: string;
+  published_date: string;
+  author: string;
+};
+
+export interface BlogPostProps {
+  title: string;
+  handlePress?: () => void;
+  blogs: BlogsProps[];
+  mainBlog: BlogsProps;
+}
+
+const BlogPosts = (props: BlogPostProps) => {
+  const intl = useIntl();
+  const itemHeight = useResponsiveValue([120, 120, 180, 240]);
+  const itemMainHeight = useResponsiveValue([240, 320, 420, 480]);
+  const [getWidth] = useDimension();
+
+  return (
+    <View style={{marginHorizontal: 10}} testID="blogPosts">
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}>
+        <Text variant="titleLarge" style={{marginBottom: 9, fontSize: 20}}>
+          {props.title}
+        </Text>
+
+        <Pressable onPress={props.handlePress}>
+          <Text
+            variant="titleMedium"
+            style={{
+              marginBottom: 9,
+              textDecorationLine: 'underline',
+              fontSize: 14,
+            }}>
+            {intl.formatMessage({defaultMessage: 'View All'})}
+          </Text>
+        </Pressable>
+      </View>
+
+      <View style={{flexDirection: getWidth < 800 ? 'column' : 'row'}}>
+        <View style={{width: getWidth < 800 ? '100%' : '40%'}}>
+          <MainBlog blog={props.mainBlog} itemHeight={itemMainHeight} />
+        </View>
+        <Divider style={{marginVertical: 5}} />
+        <View
+          style={{
+            width: getWidth < 800 ? '100%' : '58%',
+            marginHorizontal: getWidth < 800 ? 0 : 10,
+          }}>
+          {props.blogs.map(blog => {
+            return (
+              <BlogRenderer key={blog.id} blog={blog} itemHeight={itemHeight} />
+            );
+          })}
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const MainBlog = ({
+  blog,
+  itemHeight,
+}: {
+  blog: BlogsProps;
+  itemHeight: number;
+}) => {
+  return (
+    <View style={{alignItems: 'center', marginBottom: 10}}>
+      <Image
+        source={{uri: blog.image}}
+        style={{height: itemHeight, width: '100%'}}
+      />
+      <Text
+        style={{
+          fontSize: 16,
+          fontFamily: '700',
+          width: '70%',
+          textAlign: 'center',
+        }}>
+        {blog.title}
+      </Text>
+      <Text style={{fontSize: 14, fontFamily: '400'}}>
+        {blog.published_date}
+      </Text>
+    </View>
+  );
+};
+
+const BlogRenderer = ({
+  blog,
+  itemHeight,
+}: {
+  blog: BlogsProps;
+  itemHeight: number;
+}) => {
+  return (
+    <View style={{flexDirection: 'row', marginBottom: 10}}>
+      <Image
+        source={{uri: blog.image}}
+        style={{height: itemHeight, width: '40%'}}
+      />
+      <View style={{marginHorizontal: '2%', width: '56%'}}>
+        <Text style={{fontSize: 16, fontFamily: '700'}}>{blog.title}</Text>
+        <Text style={{fontSize: 14, fontFamily: '400'}}>
+          {blog.published_date}
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export default BlogPosts;
