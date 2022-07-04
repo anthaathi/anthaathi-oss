7d3f27bb46f4fae3aebb7921b67b3171c8d0d649
kedar09 <httpskedar@gmail.com>
2022-07-04 16:19:55 +0530

    Profile page - personal info, delivery address component

---
+++ apps/anthaathi-commerce-mobile-client/src/features/CMS/containers/ProfilePage/components/DeliveryAddresses/index.tsx
@@ -0,0 +1,107 @@
+import {Pressable, View} from 'react-native';
+import React from 'react';
+import {Button, Card, Divider, Text} from 'react-native-paper';
+import {useIntl} from 'react-intl';
+
+type AddressProps = {
+  firstName?: string;
+  lastName?: string;
+  companyName?: string;
+  country: string;
+  address: string;
+  apartment: string;
+  city: string;
+  landmark: string;
+  postalCode: number;
+};
+
+export interface DeliveryAddressesProps {
+  title: string;
+  handlePress?: () => void;
+  userAddress: AddressProps[];
+}
+
+const DeliveryAddresses = (props: DeliveryAddressesProps) => {
+  const intl = useIntl();
+  return (
+    <View style={{marginHorizontal: 10}} testID="deliveryAddresses">
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}>
+        <Text style={{color: '#364A15', fontSize: 16, fontWeight: '600'}}>
+          {props.title}
+        </Text>
+        <Button
+          mode="text"
+          labelStyle={{color: '#008D3E'}}
+          onPress={props.handlePress}>
+          {intl.formatMessage({defaultMessage: 'Add New'})}
+        </Button>
+      </View>
+      <Card
+        style={{
+          borderColor: '#E3E2E7',
+          borderWidth: 1,
+          borderRadius: 4,
+          marginVertical: 2,
+        }}
+        testID="personalInformation">
+        <Card.Content>
+          {props.userAddress.map((data, index) => (
+            <AddressRenderer key={index} data={data} />
+          ))}
+        </Card.Content>
+      </Card>
+    </View>
+  );
+};
+
+const AddressRenderer = ({
+  data,
+  handlePress,
+}: {
+  data: AddressProps;
+  handlePress?: () => void;
+}) => {
+  const intl = useIntl();
+  return (
+    <>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'flex-start',
+          alignItems: 'flex-start',
+          marginVertical: 5,
+        }}>
+        <View style={{width: '80%'}}>
+          <Text style={{fontSize: 14, color: '#364A15', fontWeight: '600'}}>
+            {data.apartment +
+              ', ' +
+              data.address +
+              ', ' +
+              data.city +
+              ', ' +
+              data.country}
+          </Text>
+
+          <Text style={{fontSize: 14, color: '#808080', fontWeight: '500'}}>
+            {'Landmark: ' + data.landmark}
+          </Text>
+        </View>
+        <Pressable
+          onPress={handlePress}
+          style={{width: '20%', alignItems: 'center'}}>
+          <Text style={{color: '#008D3E', fontSize: 14, fontWeight: '500'}}>
+            {intl.formatMessage({defaultMessage: 'Edit'})}
+          </Text>
+        </Pressable>
+      </View>
+      <Divider style={{marginVertical: 4}} />
+    </>
+  );
+};
+
+export default DeliveryAddresses;
---
+++ apps/anthaathi-commerce-mobile-client/src/features/CMS/containers/ProfilePage/components/PersonalInformation/index.tsx
@@ -0,0 +1,87 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+import {Button, Card} from 'react-native-paper';
+import {useIntl} from 'react-intl';
+
+type PersonalInfo = {
+  name: string;
+  email: string;
+  mobile: string;
+  alternateMobile: string;
+};
+
+export interface PersonalInformationProps {
+  title: string;
+  handlePress?: () => void;
+  personalInfo: PersonalInfo;
+}
+
+const PersonalInformation = (props: PersonalInformationProps) => {
+  const intl = useIntl();
+
+  return (
+    <Card
+      style={{
+        marginHorizontal: 10,
+        borderColor: '#E3E2E7',
+        borderWidth: 1,
+        borderRadius: 4,
+        marginVertical: 5,
+      }}
+      testID="personalInformation">
+      <Card.Content>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}>
+          <Text style={{color: '#364A15', fontSize: 16, fontWeight: '600'}}>
+            {props.title}
+          </Text>
+          <Button
+            mode="text"
+            labelStyle={{color: '#008D3E'}}
+            onPress={props.handlePress}>
+            Edit
+          </Button>
+        </View>
+        <InformationRenderer
+          label={intl.formatMessage({defaultMessage: 'Name'})}
+          name={props.personalInfo.name}
+        />
+        <InformationRenderer
+          label={intl.formatMessage({defaultMessage: 'Email'})}
+          name={props.personalInfo.email}
+        />
+        <InformationRenderer
+          label={intl.formatMessage({defaultMessage: 'Mobile'})}
+          name={props.personalInfo.mobile}
+        />
+        <InformationRenderer
+          label={intl.formatMessage({defaultMessage: 'Alternate Mobile'})}
+          name={props.personalInfo.alternateMobile}
+        />
+      </Card.Content>
+    </Card>
+  );
+};
+
+const InformationRenderer = ({label, name}: {label: string; name: string}) => {
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        marginVertical: 5,
+      }}>
+      <Text style={{color: '#808080', fontSize: 14, fontWeight: '400'}}>
+        {label + ':  '}
+      </Text>
+      <Text style={{color: '#364A15', fontSize: 14, fontWeight: '600'}}>
+        {name}
+      </Text>
+    </View>
+  );
+};
+
+export default PersonalInformation;
