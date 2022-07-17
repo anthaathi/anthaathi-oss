6cb8d9a6df0ec550dae55a7bfa4513f156e95ed1
Horizon <omkar@anthaathi.org>
2022-07-17 13:17:26 +0400

    feat: added schema generator

--- apps/anthaathi-graphql-engine/src/main/kotlin/org/anthaathi/graphqlengine/plugins/core/engine/CoreEngine.kt
+++ apps/anthaathi-graphql-engine/src/main/kotlin/org/anthaathi/graphqlengine/plugins/core/engine/CoreEngine.kt
@@ -1,20 +1,26 @@
 package org.anthaathi.graphqlengine.plugins.core.engine
 
+import com.netflix.graphql.dgs.DgsCodeRegistry
 import com.netflix.graphql.dgs.DgsComponent
 import com.netflix.graphql.dgs.DgsTypeDefinitionRegistry
 import graphql.language.*
+import graphql.schema.DataFetcher
+import graphql.schema.DataFetchingEnvironment
+import graphql.schema.FieldCoordinates
+import graphql.schema.GraphQLCodeRegistry
 import graphql.schema.idl.TypeDefinitionRegistry
 import org.anthaathi.graphqlengine.plugins.core.input_generator.*
-import org.anthaathi.graphqlengine.plugins.postgres.PostgreSQLGraphqlEngine
 import org.anthaathi.graphqlengine.plugins.core.interfaces.CorePlugin
+import org.anthaathi.graphqlengine.plugins.core.schema_generator.CRUDGenerator
+import java.util.*
+
 
 @DgsComponent
 class CoreEngine {
-    val plugins = listOf<CorePlugin>(
-        PostgreSQLGraphqlEngine()
-    )
+    val plugins = listOf<CorePlugin>()
+    val schemaGenerator = listOf(CRUDGenerator())
 
-    val inputGenerator = listOf<InputGenerator>(
+    val inputGenerator = listOf(
         StringCmp(),
         IDCmp(),
         BooleanCmp(),
@@ -27,17 +33,29 @@ class CoreEngine {
     fun registry(schemaRegistry: TypeDefinitionRegistry): TypeDefinitionRegistry? {
         var typeDefinitionRegistry = TypeDefinitionRegistry()
 
-        plugins.forEach {
+        inputGenerator.forEach {
             typeDefinitionRegistry = typeDefinitionRegistry.merge(it.registry(schemaRegistry))
         }
 
-        inputGenerator.forEach {
+        schemaGenerator.forEach {
             typeDefinitionRegistry = typeDefinitionRegistry.merge(it.registry(schemaRegistry))
         }
 
         typeDefinitionRegistry.add(createNodeQuery())
 
         return typeDefinitionRegistry
+    }
+
+    @DgsCodeRegistry
+    fun registry(
+        codeRegistryBuilder: GraphQLCodeRegistry.Builder,
+        registry: TypeDefinitionRegistry?
+    ): GraphQLCodeRegistry.Builder? {
+        val df = DataFetcher { dfe: DataFetchingEnvironment? -> Random().nextInt() }
+
+        val coordinates = FieldCoordinates.coordinates("Query", "randomNumber")
+
+        return codeRegistryBuilder.dataFetcher(coordinates, df)
     }
 
     private fun createNodeQuery(): ObjectTypeDefinition {
