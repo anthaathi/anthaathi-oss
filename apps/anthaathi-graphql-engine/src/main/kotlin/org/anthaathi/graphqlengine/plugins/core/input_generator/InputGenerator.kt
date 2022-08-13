1309acb1834f32b0ddd53155e9e2c0732f28ca10
Omkar <me@f22.dev>
2022-08-13 10:53:47 +0400

    fix: test cases

--- apps/anthaathi-graphql-engine/src/main/kotlin/org/anthaathi/graphqlengine/plugins/core/input_generator/InputGenerator.kt
+++
@@ -1,7 +0,0 @@
-package org.anthaathi.graphqlengine.plugins.core.input_generator
-
-import graphql.schema.idl.TypeDefinitionRegistry
-
-interface InputGenerator {
-    fun registry(schemaRegistry: TypeDefinitionRegistry): TypeDefinitionRegistry?
-}
