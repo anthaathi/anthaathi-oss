25cabf707f9306fa457f64dea41acbd99ace023c
kedar09 <httpskedar@gmail.com>
2022-08-24 11:01:38 +0530

    design update - calendar & task list

--- apps/anthaathi-crm-web-client/src/Features/MiniActionbar/Components/CalendarAction/index.tsx
+++ apps/anthaathi-crm-web-client/src/Features/MiniActionbar/Components/CalendarAction/index.tsx
@@ -26,7 +26,7 @@ export function CalendarAction() {
         className={css({
           width: `calc(100% + ${$theme.sizing.scale400} + ${$theme.sizing.scale400})`,
           marginTop: $theme.sizing.scale600,
-          marginBottom: $theme.sizing.scale500,
+          marginBottom: $theme.sizing.scale100,
           ...expandBorderStyles($theme.borders.border100),
         })}
       />
@@ -38,14 +38,40 @@ export function CalendarAction() {
               style: () => ({ height: '42px' }),
             },
             Day: {
-              style: ({ $theme, $selected, $isHovered, $isHighlighted }) => ({
+              style: ({ $selected, $isHovered, $isHighlighted }) => ({
                 width: '42px',
                 height: '42px',
+                color: $selected
+                  ? '#fff'
+                  : $isHovered || $isHighlighted
+                  ? '#0077cc'
+                  : '#000',
+                ':hover': {
+                  transform: 'scale(1.05)',
+                  transitionProperty: 'all',
+                  transitionDuration: '90ms',
+                  transitionTimingFunction: 'ease-in-out',
+                  textDecoration: 'underline',
+                  textUnderlineOffset: '2px',
+                },
                 ':after': {
-                  marginTop: '4px',
-                  marginLeft: '1px',
-                  width: '40px',
-                  height: '40px',
+                  marginTop: '5px',
+                  marginLeft: '2px',
+                  width: '38px',
+                  height: '38px',
+                  borderTopLeftRadius: '4px',
+                  borderTopRightRadius: '4px',
+                  borderBottomLeftRadius: '4px',
+                  borderBottomRightRadius: '4px',
+                  backgroundColor: $selected
+                    ? '#0077cc'
+                    : $isHovered || $isHighlighted
+                    ? '#0077cc33'
+                    : 'transparent',
+                  borderTopWidth: '0px',
+                  borderBottomWidth: '0px',
+                  borderLeftWidth: '0px',
+                  borderRightWidth: '0px',
                 },
               }),
             },
@@ -53,6 +79,22 @@ export function CalendarAction() {
               style: () => ({
                 width: '42px',
                 height: '42px',
+              }),
+            },
+            NextButton: {
+              style: () => ({
+                ':hover': {
+                  backgroundColor: '#0077cc33',
+                  borderRadius: '4px',
+                },
+              }),
+            },
+            PrevButton: {
+              style: () => ({
+                ':hover': {
+                  backgroundColor: '#0077cc33',
+                  borderRadius: '4px',
+                },
               }),
             },
           }}
