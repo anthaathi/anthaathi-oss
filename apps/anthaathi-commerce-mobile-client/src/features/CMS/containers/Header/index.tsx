baae6bc2cdf30177379b56bb66b4e5ab566e5c26
kedar09 <kedar@anthaathi.org>
2022-09-08 21:30:53 +0530

    Header and bottom bar issue

--- apps/anthaathi-commerce-mobile-client/src/features/CMS/containers/Header/index.tsx
+++ apps/anthaathi-commerce-mobile-client/src/features/CMS/containers/Header/index.tsx
@@ -1,88 +1,219 @@
-import React from 'react';
-import {Image, StatusBar, StyleSheet, View} from 'react-native';
-import {Appbar, useTheme} from 'react-native-paper';
-import {CoreComponentType} from '../../types/common';
+import React, { useState } from 'react';
+import { I18nManager, Image, Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { Appbar, useTheme, TextInput, Colors, Badge } from 'react-native-paper';
+import { CoreComponentType } from '../../types/common';
+import RNRestart from "react-native-restart";
+import ChatWootWidget from "@chatwoot/react-native-widget";
+import { useRecoilValue } from 'recoil';
+import { CartItemData } from '../../context/CartItemContext';
 
 export interface HeaderProps {
-  rightIcon?: string;
-  rightOnPress?: () => void;
-  leftIcon?: string;
-  leftOnPress?: () => void;
-  title?: string;
-  placement?: 'left' | 'center';
-  logoImage?: string;
+    leftIcon?: string;
+    leftOnPress?: () => void;
+    cartIcon?: boolean;
+    cartOnPress?: () => void;
+    languageIcon?: boolean;
+    mailIcon?: boolean;
+    // translateOnPress?: () => void;
+    searchIcon?: boolean;
+    title?: string;
+    logoImage?: string;
+    isInlineSearch?: boolean;
 }
 
 const Header = ({
-  rightIcon,
-  rightOnPress,
-  leftIcon,
-  leftOnPress,
-  title = '',
-  placement = 'left',
-  logoImage,
+    leftIcon,
+    leftOnPress,
+    cartIcon = false,
+    cartOnPress,
+    languageIcon = false,
+    mailIcon = false,
+    searchIcon = false,
+    title = '',
+    logoImage,
+    isInlineSearch = false
 }: HeaderProps) => {
-  const theme = useTheme();
-  return (
-    <View testID="header">
-      <StatusBar
-        animated={true}
-        backgroundColor={theme.colors.primary}
-        barStyle={theme.dark ? 'dark-content' : 'light-content'}
-      />
-      <Appbar.Header
-        style={{
-          backgroundColor: theme.colors.background,
-        }}>
-        {leftIcon && (
-          <Appbar.Action
-            testID="leftOnHandler"
-            icon={leftIcon}
-            color="#364A15"
-            onPress={leftOnPress}
-          />
-        )}
-        {title === '' ? (
-          <Image
-            testID="headerImage"
-            style={[
-              {
-                height: 48,
-                width: 70,
-              },
-              placement === 'center' && styles.headerAlign,
-            ]}
-            source={{
-              uri: logoImage,
-            }}
-          />
-        ) : null}
-        <Appbar.Content
-          title={title}
-          style={[placement === 'center' && styles.headerAlign]}
-        />
-        {rightIcon && (
-          <Appbar.Action
-            testID="rightOnHandler"
-            icon={rightIcon}
-            color="#364A15"
-            onPress={rightOnPress}
-          />
-        )}
-      </Appbar.Header>
-    </View>
-  );
+    const [searchShow, setSearchShow] = React.useState(false);
+    const [showWidget, toggleWidget] = useState(false);
+    const theme = useTheme();
+    const cartItem = useRecoilValue(CartItemData);
+
+    const user = {
+        identifier: 'john@gmail.com',
+        name: 'John Samuel',
+        avatar_url: '',
+        email: 'john@gmail.com',
+        identifier_hash: '',
+    };
+    const customAttributes = {
+        accountId: 1,
+        pricingPlan: 'paid',
+        status: 'active',
+    };
+
+    const websiteToken = 'bsUQcgVTRZ7JGiuXZbeTCGGE';
+    const baseUrl = 'https://app.chatwoot.com';
+    const locale = 'en';
+
+    async function changeLanguage() {
+        if (I18nManager.isRTL) {
+            await I18nManager.forceRTL(false);
+        } else {
+            if (!I18nManager.isRTL) {
+                await I18nManager.forceRTL(true);
+            }
+        }
+        RNRestart.Restart();
+    }
+
+    return (
+        <View testID="header" style={{ backgroundColor: '#fff', elevation: 1 }}>
+            <StatusBar
+                animated={true}
+                backgroundColor={theme.colors.primary}
+                barStyle={theme.dark || Platform.OS === 'ios' ? 'dark-content' : 'light-content'}
+            />
+            <Appbar.Header
+                statusBarHeight={Platform.OS === 'ios' ? 20 : 0}
+                style={{
+                    height: 56,
+                    backgroundColor: '#fff',// theme.colors.background,
+                    elevation: 0,
+                }}
+            >
+                {leftIcon && (
+                    <Appbar.Action
+                        testID="leftOnHandler"
+                        icon={leftIcon}
+                        color={Colors.grey800}
+                        onPress={leftOnPress}
+                    />
+                )}
+                {languageIcon && (
+                    <Appbar.Action
+                        testID="translateOnHandler"
+                        icon={'translate'}
+                        color={Colors.grey800}
+                        onPress={() => {
+                            changeLanguage()
+                        }}
+                    />
+                )}
+
+                {logoImage && (
+                    <>
+                        <Appbar.Content
+                            title={''}
+                            style={{
+                                alignItems: 'center',
+                            }}
+                        />
+                        <Image
+                            testID="headerImage"
+                            style={[
+                                {
+                                    height: 52,
+                                    width: 70,
+                                    // alignItems: 'center'
+                                    // marginLeft: leftIcon ? '14%' : '40%',
+                                },
+                            ]}
+                            source={{
+                                uri: logoImage,
+                            }}
+                        />
+                    </>
+                )}
+
+
+                <Appbar.Content
+                    title={title}
+                />
+
+                {mailIcon && (
+                    <Appbar.Action
+                        onPress={() => toggleWidget(true)}
+                        color={Colors.grey800}
+                        icon="email-outline"
+                    />
+                )}
+
+                {cartIcon && (
+                    <View>
+                        <Appbar.Action
+                            testID="cartOnHandler"
+                            icon={'shopping'}
+                            color={Colors.grey800}
+                            onPress={cartOnPress}
+                        />
+                        {cartItem.length > 0 && (
+                            <Badge
+                                style={{
+                                    backgroundColor: Colors.grey800,
+                                    position: 'absolute',
+                                    top: 6,
+                                }}>
+                                {cartItem.length}
+                            </Badge>
+                        )}
+                    </View>
+                )}
+
+                {searchIcon && (
+                    <Appbar.Action
+                        testID="searchOnHandler"
+                        icon={'magnify'}
+                        color={Colors.grey800}
+                        onPress={() => {
+                            setSearchShow(!searchShow)
+                        }}
+                    />
+                )}
+            </Appbar.Header>
+            {(searchShow || isInlineSearch) &&
+                <View
+                    style={[
+                        I18nManager.isRTL ? styles.containerRTL : styles.containerLTR,
+                        { paddingTop: 2, paddingBottom: 10 },
+                    ]}>
+                    <TextInput placeholder="Search" dense underlineColor="transparent" />
+                </View>
+            }
+
+            {showWidget && (
+                <ChatWootWidget
+                    websiteToken={websiteToken}
+                    locale={locale}
+                    baseUrl={baseUrl}
+                    closeModal={() => toggleWidget(false)}
+                    isModalVisible={showWidget}
+                    user={user}
+                    customAttributes={customAttributes}
+                />
+            )}
+        </View>
+    );
+
 };
 
 export default Header;
 
 const styles = StyleSheet.create({
-  headerAlign: {
-    alignItems: 'center',
-  },
+    headerAlign: {
+        alignItems: 'center',
+    },
+    containerLTR: {
+        paddingRight: 24,
+        paddingLeft: 8,
+    },
+    containerRTL: {
+        paddingLeft: 48,
+        paddingRight: 0,
+    },
+
 });
 
 export const HeaderCMSInput = {
-  _component: CoreComponentType.Header,
-  component: Header,
+    _component: CoreComponentType.Header,
+    component: Header,
 };
--- apps/anthaathi-commerce-mobile-client/src/pages/AddressDetails/AddressInfoPage/index.tsx
+++ apps/anthaathi-commerce-mobile-client/src/pages/AddressDetails/AddressInfoPage/index.tsx
@@ -5,27 +5,40 @@ import CMSRenderer from '../../../features/CMS';
 import dataJson from '../../../config/data.json';
 import {RootStackParamList} from '../../../types/Route';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import Header from "../../../features/CMS/containers/Header";
 
 const AddressInfoPage = (
-  props: NativeStackScreenProps<RootStackParamList, 'AddressInfo'>,
+    props: NativeStackScreenProps<RootStackParamList, 'AddressInfo'>,
 ) => {
-  return (
-    <View style={{marginVertical: 10}}>
-      <CMSRenderer
-        components={[
-          {
-            _component: ProfilePageComponentType.DeliveryAddresses,
-            key: '3232',
-            title: 'Address information',
-            userAddress: dataJson.core.profilePage.addressInformation,
-            handlePress: () => {
-              props.navigation.navigate('AddEditAddress');
-            },
-          },
-        ]}
-      />
-    </View>
-  );
+    return (
+        <View>
+            <Header
+                leftIcon={'arrow-left'}
+                leftOnPress={() => props.navigation.goBack()}
+                languageIcon={true}
+                cartIcon={true}
+                cartOnPress={() => {
+                    props.navigation.navigate('CartPage');
+                }}
+                mailIcon={false}
+                searchIcon={true}
+                logoImage={'https://cdn.shopify.com/s/files/1/0648/1303/9842/files/everyday_1_256x256.png?v=1662529180'}
+            />
+            <CMSRenderer
+                components={[
+                    {
+                        _component: ProfilePageComponentType.DeliveryAddresses,
+                        key: '3232',
+                        title: 'Address information',
+                        userAddress: dataJson.core.profilePage.addressInformation,
+                        handlePress: () => {
+                            props.navigation.navigate('AddEditAddress');
+                        },
+                    },
+                ]}
+            />
+        </View>
+    );
 };
 
 export default AddressInfoPage;
