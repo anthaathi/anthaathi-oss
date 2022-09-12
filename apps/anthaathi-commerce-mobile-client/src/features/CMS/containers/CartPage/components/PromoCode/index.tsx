9be51899e5b276e5480660a6b859530f4d00a052
kedar09 <kedar@anthaathi.org>
2022-09-12 16:56:16 +0530

    modal - promo code & ios - date picker design updates

--- apps/anthaathi-commerce-mobile-client/src/features/CMS/containers/CartPage/components/PromoCode/index.tsx
+++ apps/anthaathi-commerce-mobile-client/src/features/CMS/containers/CartPage/components/PromoCode/index.tsx
@@ -1,42 +1,120 @@
-import React from 'react';
-import {Pressable, View} from 'react-native';
-import {Text, useTheme} from 'react-native-paper';
+import React, {useState} from 'react';
+import {Modal, Pressable, StyleSheet, View} from 'react-native';
+import {Button, Text, TextInput, useTheme} from 'react-native-paper';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {MD3Colors} from 'react-native-paper/lib/typescript/types';
 import {CartPageComponentType} from '../../../../types/common';
 
 export interface PromoCodeProps {
   title: string;
-  handlePress?: () => void;
 }
 
 const PromoCode = (props: PromoCodeProps) => {
   const theme = useTheme();
+  const [modalVisible, setModalVisible] = useState(false);
   return (
-    <View
-      style={{alignItems: 'center', marginVertical: 5, marginHorizontal: 10}}
-      testID="promoCode">
-      <Pressable
-        testID="handlePress"
-        onPress={props.handlePress}
-        style={{
-          backgroundColor: (theme.colors as MD3Colors).primaryContainer,
-          width: '100%',
-          height: 48,
-          borderRadius: 4,
-          justifyContent: 'center',
-        }}>
-        <View
+    <View testID="promoCode">
+      <View
+        style={{alignItems: 'center', marginVertical: 5, marginHorizontal: 10}}>
+        <Pressable
+          testID="handlePress"
+          onPress={() => {
+            setModalVisible(!modalVisible);
+          }}
           style={{
-            flexDirection: 'row',
-            justifyContent: 'space-around',
+            backgroundColor: (theme.colors as MD3Colors).primaryContainer,
+            width: '100%',
+            height: 48,
+            borderRadius: 4,
+            justifyContent: 'center',
           }}>
-          <Text style={{color: '#737272', fontSize: 14, fontWeight: '500'}}>
-            {props.title}
-          </Text>
-          <Icon name="tag" color={theme.colors.primary} size={20} />
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-around',
+            }}>
+            <Text style={{color: '#737272', fontSize: 14, fontWeight: '500'}}>
+              {props.title}
+            </Text>
+            <Icon name="tag" color={theme.colors.primary} size={20} />
+          </View>
+        </Pressable>
+      </View>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+        <Pressable
+          style={StyleSheet.flatten([
+            {
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: 'rgba(0, 0, 0, .4)',
+            },
+          ])}
+          onPress={() => {
+            setModalVisible(!modalVisible);
+          }}
+        />
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={{color: '#364A15', fontSize: 16, fontWeight: '700'}}>
+              Add New Coupon Code
+            </Text>
+
+            <TextInput
+              mode="flat"
+              label={'Coupon Code'}
+              style={{
+                backgroundColor: '#fff',
+                fontSize: 14,
+                width: '100%',
+                height: 56,
+                marginVertical: 10,
+              }}
+              activeUnderlineColor="#0f8443"
+            />
+
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-around',
+                marginTop: 15,
+              }}>
+              <Button
+                mode="outlined"
+                style={{
+                  borderRadius: 4,
+                  borderColor: theme.colors.primary,
+                }}
+                onPress={() => {
+                  setModalVisible(!modalVisible);
+                }}>
+                Cancel
+              </Button>
+              <Button
+                mode="contained"
+                style={{
+                  borderRadius: 4,
+                }}
+                onPress={() => {
+                  setModalVisible(!modalVisible);
+                }}>
+                Apply
+              </Button>
+            </View>
+          </View>
         </View>
-      </Pressable>
+      </Modal>
     </View>
   );
 };
@@ -47,3 +125,46 @@ export const PromoCodeCMSInput = {
   _component: CartPageComponentType.PromoCode,
   component: PromoCode,
 };
+
+const styles = StyleSheet.create({
+  centeredView: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  modalView: {
+    width: '80%',
+    paddingHorizontal: 15,
+    paddingVertical: 20,
+    backgroundColor: 'white',
+    borderRadius: 4,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
--- apps/anthaathi-commerce-mobile-client/src/features/CMS/containers/Core/components/DatePicker/index.tsx
+++ apps/anthaathi-commerce-mobile-client/src/features/CMS/containers/Core/components/DatePicker/index.tsx
@@ -1,62 +1,124 @@
-import {View, Text} from 'react-native';
+import {View, Text, Platform, TouchableOpacity} from 'react-native';
 import React from 'react';
 
 import DateTimePicker from '@react-native-community/datetimepicker';
-import {Button} from 'react-native-paper';
+import {Button, Divider} from 'react-native-paper';
 import {CoreComponentType} from '../../../../types/common';
+import CMSBottomSheet from "../CMSBottomSheet";
 
-const DatePicker = (props: {title: string}) => {
-  const [open, setOpen] = React.useState(false);
-  const [date, setDate] = React.useState(new Date());
+const DatePicker = (props: { title: string }) => {
+    const [open, setOpen] = React.useState(false);
+    const [date, setDate] = React.useState(new Date());
 
-  const formatDate = (dateData: Date) => {
-    return `${dateData.getDate()}/${
-      dateData.getMonth() + 1
-    }/${dateData.getFullYear()}`;
-  };
+    const formatDate = (dateData: Date) => {
+        return `${dateData.getDate()}/${
+            dateData.getMonth() + 1
+        }/${dateData.getFullYear()}`;
+    };
 
-  return (
-    <View testID="datePicker" style={{marginHorizontal: 10, marginVertical: 5}}>
-      <Text style={{color: '#364A15', fontSize: 16, fontWeight: '600'}}>
-        {props.title}
-      </Text>
+    return (
+        <View testID="datePicker" style={{marginHorizontal: 10, marginVertical: 5}}>
+            <Text style={{color: '#364A15', fontSize: 16, fontWeight: '600'}}>
+                {props.title}
+            </Text>
 
-      <Button
-        style={{
-          marginVertical: 3,
-          borderColor: '#E3E2E7',
-          backgroundColor: '#fff',
-        }}
-        contentStyle={{
-          paddingVertical: 5,
-          flexDirection: 'row-reverse',
-          justifyContent: 'space-between',
-        }}
-        labelStyle={{color: '#364A15'}}
-        icon="pencil"
-        onPress={() => setOpen(true)}
-        uppercase={false}
-        mode="outlined">
-        {formatDate(date)}
-      </Button>
-      {open && (
-        <DateTimePicker
-          mode="date"
-          value={date}
-          onChange={(_e, d) => {
-            const currentDate = d || new Date();
-            setDate(currentDate);
-            setOpen(false);
-          }}
-        />
-      )}
-    </View>
-  );
+            <Button
+                style={{
+                    marginVertical: 3,
+                    borderColor: '#E3E2E7',
+                    backgroundColor: '#fff',
+                }}
+                contentStyle={{
+                    paddingVertical: 5,
+                    flexDirection: 'row-reverse',
+                    justifyContent: 'space-between',
+                }}
+                labelStyle={{color: '#364A15'}}
+                icon="pencil"
+                onPress={() => setOpen(true)}
+                uppercase={false}
+                mode="outlined">
+                {formatDate(date)}
+            </Button>
+
+            {Platform.OS === "ios" && (
+                <CMSBottomSheet
+                    bottomSheetTitle={"Select Date"}
+                    bottomSheetIconColor="#0A2463"
+                    bottomSheetStyle={{
+                        backgroundColor: "white",
+                    }}
+                    bottomSheetTitleStyle={{color: "#0A2463"}}
+                    setBottomSheetVisible={setOpen}
+                    bottomSheetVisible={open}
+                >
+                    <View>
+                        <Divider/>
+                        <DateTimePicker
+                            mode="date"
+                            display={"spinner"}
+                            value={date}
+                            onChange={(_e, d) => {
+                                const currentDate = d || new Date();
+                                setDate(currentDate);
+                                setOpen(false);
+                            }}
+                        />
+                        <Divider/>
+                    </View>
+                    <View
+                        style={{flexDirection: "row", justifyContent: "space-between"}}
+                    >
+                        <TouchableOpacity
+                            style={{
+                                paddingVertical: 15,
+                                alignItems: "center",
+                                width: "50%",
+                            }}
+                            onPress={() => setOpen(!open)}
+                        >
+                            <Text
+                                style={{fontSize: 16, fontWeight: "600", color: "#ff3d33"}}
+                            >
+                                Cancel
+                            </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            style={{
+                                paddingVertical: 15,
+                                alignItems: "center",
+                                width: "50%",
+                            }}
+                            onPress={() => setOpen(!open)}
+                        >
+                            <Text
+                                style={{fontSize: 16, fontWeight: "600", color: "#017aff"}}
+                            >
+                                Confirm
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </CMSBottomSheet>
+            )}
+
+            {Platform.OS === "android" && open && (
+                <DateTimePicker
+                    mode="date"
+                    value={date}
+                    onChange={(_e, d) => {
+                        const currentDate = d || new Date();
+                        setDate(currentDate);
+                        setOpen(false);
+                    }}
+                />
+            )}
+        </View>
+    );
 };
 
 export default DatePicker;
 
 export const DatePickerCMSInput = {
-  _component: CoreComponentType.DatePicker,
-  component: DatePicker,
+    _component: CoreComponentType.DatePicker,
+    component: DatePicker,
 };
