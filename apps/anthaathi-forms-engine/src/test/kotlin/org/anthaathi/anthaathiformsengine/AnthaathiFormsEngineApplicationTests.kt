c3ce68c17ccd67cb13078203f8340ed8bf580021
Horizon <omkar@anthaathi.org>
2022-09-14 21:24:57 +0530

    feat: basic implementation for the form

---
+++ apps/anthaathi-forms-engine/src/test/kotlin/org/anthaathi/anthaathiformsengine/AnthaathiFormsEngineApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.anthaathi.anthaathiformsengine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AnthaathiFormsEngineApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
