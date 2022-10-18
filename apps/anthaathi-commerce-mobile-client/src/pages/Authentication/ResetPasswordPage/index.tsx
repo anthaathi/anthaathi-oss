e6fc1c98ea9edde5958cb7459d94dc11bf4500ca
sourabh1997 <sourabh@anthaathi.org>
2022-10-18 18:25:44 +0530

    keyboardavoidingview in sign up, reset password, sign in and checkout page and testflight version update

--- apps/anthaathi-commerce-mobile-client/src/pages/Authentication/ResetPasswordPage/index.tsx
+++ apps/anthaathi-commerce-mobile-client/src/pages/Authentication/ResetPasswordPage/index.tsx
@@ -1,4 +1,4 @@
-import {View, ScrollView, Text} from 'react-native';
+import {View, ScrollView, Text, KeyboardAvoidingView, Platform} from 'react-native';
 import React from 'react';
 import {Button, TextInput, useTheme} from 'react-native-paper';
 import {RootStackParamList} from '../../../types/Route';
@@ -12,61 +12,66 @@ const ResetPasswordPage = (
   const intl = useIntl();
 
   return (
-    <View
-      style={{
-        backgroundColor: '#fff',
-        flex: 1,
-      }}>
-      <ScrollView style={{paddingHorizontal: 25}}>
-        <View
-          style={{
-            paddingTop: '45%',
-            marginBottom: 20,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-          <Text
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      enabled
+      style={{flex: 1}}>
+      <View
+        style={{
+          backgroundColor: '#fff',
+          flex: 1,
+        }}>
+        <ScrollView style={{paddingHorizontal: 25}}>
+          <View
             style={{
-              color: theme.colors.primary,
-              fontWeight: '700',
-              fontSize: 28,
+              paddingTop: '45%',
+              marginBottom: 20,
+              alignItems: 'center',
+              justifyContent: 'center',
             }}>
-            {intl.formatMessage({defaultMessage: 'Reset your password'})}
-          </Text>
-        </View>
-        <View style={{marginVertical: 15}}>
-          <TextInput
-            mode="flat"
-            label={intl.formatMessage({
-              defaultMessage: 'Username or email address',
-            })}
-            style={{
-              backgroundColor: '#fff',
-              fontSize: 14,
-              height: 56,
-              marginHorizontal: 5,
-              marginVertical: 10,
-            }}
-            activeUnderlineColor={theme.colors.primary}
-          />
-        </View>
+            <Text
+              style={{
+                color: theme.colors.primary,
+                fontWeight: '700',
+                fontSize: 28,
+              }}>
+              {intl.formatMessage({defaultMessage: 'Reset your password'})}
+            </Text>
+          </View>
+          <View style={{marginVertical: 15}}>
+            <TextInput
+              mode="flat"
+              label={intl.formatMessage({
+                defaultMessage: 'Username or email address',
+              })}
+              style={{
+                backgroundColor: '#fff',
+                fontSize: 14,
+                height: 56,
+                marginHorizontal: 5,
+                marginVertical: 10,
+              }}
+              activeUnderlineColor={theme.colors.primary}
+            />
+          </View>
 
-        <Button
-          mode="contained"
-          uppercase={false}
-          style={{
-            marginVertical: 15,
-            marginHorizontal: 30,
-            borderRadius: 32,
-          }}
-          labelStyle={{paddingVertical: 5}}
-          onPress={() => {
-            props.navigation.navigate('SignIn');
-          }}>
-          {intl.formatMessage({defaultMessage: 'Get password link'})}
-        </Button>
-      </ScrollView>
-    </View>
+          <Button
+            mode="contained"
+            uppercase={false}
+            style={{
+              marginVertical: 15,
+              marginHorizontal: 30,
+              borderRadius: 32,
+            }}
+            labelStyle={{paddingVertical: 5}}
+            onPress={() => {
+              props.navigation.navigate('SignIn');
+            }}>
+            {intl.formatMessage({defaultMessage: 'Get password link'})}
+          </Button>
+        </ScrollView>
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
--- apps/anthaathi-commerce-mobile-client/src/pages/CheckoutPage/index.tsx
+++ apps/anthaathi-commerce-mobile-client/src/pages/CheckoutPage/index.tsx
@@ -4,13 +4,19 @@ import {
   CheckOutPageComponentType,
   CoreComponentType,
 } from '../../features/CMS/types/common';
-import {ScrollView, Text, View} from 'react-native';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  Text,
+  View,
+} from 'react-native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../../types/Route';
 import {OptionDataProps} from '../../features/CMS/containers/Core/components/CMSSelectOption';
 import {TextInput, useTheme} from 'react-native-paper';
 import {useSetRecoilState} from 'recoil';
-import { CartItemData } from '../../hooks/useCart';
+import {CartItemData} from '../../hooks/useCart';
 
 const CheckoutPage: React.FC<
   NativeStackScreenProps<RootStackParamList, 'CheckoutPage'>
@@ -18,178 +24,184 @@ const CheckoutPage: React.FC<
   const theme = useTheme();
   const setCartItem = useSetRecoilState(CartItemData);
   return (
-    <View style={{flex: 1}}>
-      <CMSRenderer
-        components={[
-          {
-            _component: CoreComponentType.Header,
-            key: '142',
-            title: 'Checkout',
-            leftIcon: 'close',
-            leftOnPress: () => {
-              props.navigation.goBack();
-            },
-          },
-        ]}
-      />
-      <ScrollView
-        style={{
-          paddingHorizontal: 10,
-          flex: 1,
-          marginBottom: 60,
-        }}>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      enabled
+      style={{flex: 1}}>
+      <View style={{flex: 1}}>
         <CMSRenderer
           components={[
-            // {
-            //   _component: CheckOutPageComponentType.DeliveryAddressCard,
-            //   key: '1',
-            //   deliveryTitle: 'Delivery Address',
-            //   deliveryAddress:
-            //     'Flat 13B, Prestine Towers, Downtown Town, Dubai, UAE - 72001',
-            //   buttonTitle: 'Change',
-            //   mobileNumber: '+90909090',
-            // },
             {
-              _component: CoreComponentType.CMSSelectOption,
-              key: '21',
-              title: 'Delivery Address',
-              options: [
-                {
-                  id: 1,
-                  key: 1,
-                  title: 'Apartment',
-                  subtitle: '14b street, AI Quoz Industrial Area 4',
-                  leftIconName: 'map-marker',
-                },
-                {
-                  id: 2,
-                  key: 2,
-                  title: 'Apartment',
-                  subtitle: '1A street, Discovery Gardens',
-                  leftIconName: 'map-marker',
-                },
-                {
-                  id: 3,
-                  key: 3,
-                  title: 'Building No. 17',
-                  subtitle: '14b street, AI Quoz Industrial Area 4',
-                  leftIconName: 'map-marker',
-                },
-                {
-                  id: 4,
-                  key: 4,
-                  title: 'Apartment',
-                  subtitle: '1A street, Discovery Gardens',
-                  leftIconName: 'map-marker',
-                },
-              ],
-            },
-            {
-              _component: CoreComponentType.DatePicker,
-              key: '1412',
-              title: 'Date',
-            },
-            {
-              _component: CheckOutPageComponentType.TimeSlotSelection,
-              key: '123',
-              title: 'Timeslot',
-              timeSlots: [
-                {
-                  key: '1',
-                  name: '09am - 12pm',
-                },
-                {
-                  key: '2',
-                  name: '01pm - 05pm',
-                },
-                {
-                  key: '3',
-                  name: '05pm - 10pm',
-                },
-              ],
-            },
-            {
-              _component: CheckOutPageComponentType.TimeSlotSelection,
-              key: '12311',
-              title: 'Frequency',
-              timeSlots: [
-                {
-                  key: '1',
-                  name: 'One time',
-                },
-                {
-                  key: '2',
-                  name: 'Weekly',
-                },
-                {
-                  key: '3',
-                  name: 'Monthly',
-                },
-              ],
-            },
-            {
-              _component: CheckOutPageComponentType.PaymentMethodSelection,
-              key: '12',
-              title: 'Payment Method',
-              options: [
-                {
-                  key: '1',
-                  name: 'Credit / Debit Card',
-                },
-                {
-                  key: '2',
-                  name: 'Cash on delivery',
-                },
-                {
-                  key: '3',
-                  name: 'Wallet',
-                },
-                {
-                  key: '4',
-                  name: 'Card on delivery',
-                },
-              ],
+              _component: CoreComponentType.Header,
+              key: '142',
+              title: 'Checkout',
+              leftIcon: 'close',
+              leftOnPress: () => {
+                props.navigation.goBack();
+              },
             },
           ]}
         />
-        <View style={{marginHorizontal: 10}}>
-          <Text
-            style={{
-              color: theme.colors.titleTextColor,
-              fontSize: 16,
-              fontWeight: '600',
-            }}>
-            Comments
-          </Text>
-
-          <TextInput
-            mode="flat"
-            label={'Add a note to your order'}
-            style={{
-              backgroundColor: '#fff',
-              fontSize: 14,
-              height: 120,
-              marginVertical: 10,
-            }}
-            multiline={true}
-            activeUnderlineColor={theme.colors.primary}
+        <ScrollView
+          contentContainerStyle={{flexGrow: 1}}
+          style={{
+            paddingHorizontal: 10,
+            flex: 1,
+            marginBottom: 60,
+          }}>
+          <CMSRenderer
+            components={[
+              // {
+              //   _component: CheckOutPageComponentType.DeliveryAddressCard,
+              //   key: '1',
+              //   deliveryTitle: 'Delivery Address',
+              //   deliveryAddress:
+              //     'Flat 13B, Prestine Towers, Downtown Town, Dubai, UAE - 72001',
+              //   buttonTitle: 'Change',
+              //   mobileNumber: '+90909090',
+              // },
+              {
+                _component: CoreComponentType.CMSSelectOption,
+                key: '21',
+                title: 'Delivery Address',
+                options: [
+                  {
+                    id: 1,
+                    key: 1,
+                    title: 'Apartment',
+                    subtitle: '14b street, AI Quoz Industrial Area 4',
+                    leftIconName: 'map-marker',
+                  },
+                  {
+                    id: 2,
+                    key: 2,
+                    title: 'Apartment',
+                    subtitle: '1A street, Discovery Gardens',
+                    leftIconName: 'map-marker',
+                  },
+                  {
+                    id: 3,
+                    key: 3,
+                    title: 'Building No. 17',
+                    subtitle: '14b street, AI Quoz Industrial Area 4',
+                    leftIconName: 'map-marker',
+                  },
+                  {
+                    id: 4,
+                    key: 4,
+                    title: 'Apartment',
+                    subtitle: '1A street, Discovery Gardens',
+                    leftIconName: 'map-marker',
+                  },
+                ],
+              },
+              {
+                _component: CoreComponentType.DatePicker,
+                key: '1412',
+                title: 'Date',
+              },
+              {
+                _component: CheckOutPageComponentType.TimeSlotSelection,
+                key: '123',
+                title: 'Timeslot',
+                timeSlots: [
+                  {
+                    key: '1',
+                    name: '09am - 12pm',
+                  },
+                  {
+                    key: '2',
+                    name: '01pm - 05pm',
+                  },
+                  {
+                    key: '3',
+                    name: '05pm - 10pm',
+                  },
+                ],
+              },
+              {
+                _component: CheckOutPageComponentType.TimeSlotSelection,
+                key: '12311',
+                title: 'Frequency',
+                timeSlots: [
+                  {
+                    key: '1',
+                    name: 'One time',
+                  },
+                  {
+                    key: '2',
+                    name: 'Weekly',
+                  },
+                  {
+                    key: '3',
+                    name: 'Monthly',
+                  },
+                ],
+              },
+              {
+                _component: CheckOutPageComponentType.PaymentMethodSelection,
+                key: '12',
+                title: 'Payment Method',
+                options: [
+                  {
+                    key: '1',
+                    name: 'Credit / Debit Card',
+                  },
+                  {
+                    key: '2',
+                    name: 'Cash on delivery',
+                  },
+                  {
+                    key: '3',
+                    name: 'Wallet',
+                  },
+                  {
+                    key: '4',
+                    name: 'Card on delivery',
+                  },
+                ],
+              },
+            ]}
           />
-        </View>
-      </ScrollView>
-      <CMSRenderer
-        components={[
-          {
-            _component: CoreComponentType.CMSButton,
-            key: '1241',
-            title: 'Purchase',
-            handlePress: () => {
-              setCartItem([]);
-              props.navigation.navigate('HomePage');
+          <View style={{marginHorizontal: 10}}>
+            <Text
+              style={{
+                color: theme.colors.titleTextColor,
+                fontSize: 16,
+                fontWeight: '600',
+              }}>
+              Comments
+            </Text>
+
+            <TextInput
+              mode="flat"
+              label={'Add a note to your order'}
+              style={{
+                backgroundColor: '#fff',
+                fontSize: 14,
+                height: 120,
+                marginVertical: 10,
+              }}
+              multiline={true}
+              activeUnderlineColor={theme.colors.primary}
+            />
+          </View>
+        </ScrollView>
+        <CMSRenderer
+          components={[
+            {
+              _component: CoreComponentType.CMSButton,
+              key: '1241',
+              title: 'Purchase',
+              handlePress: () => {
+                setCartItem([]);
+                props.navigation.navigate('HomePage');
+              },
             },
-          },
-        ]}
-      />
-    </View>
+          ]}
+        />
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
